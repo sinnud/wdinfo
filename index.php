b5fd4915ceeead36f714a5367df331f04bfb025a
Luke Du <sinnud@gmail.com>
2020-05-16 18:22:14 -0700

    first release

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>WD net drive 192.168.1.243 info</h1>
+
+<?php
+if (file_exists('php_utils/wddir_default.php') ){
+
+}
+?>
+
+</body>
+</html> 
---
+++ php_utils/wddir_default.php
@@ -0,0 +1,10 @@
+<!--
+# radio form of:
+# default directory list on WD net drive
+# If later refresh, use wddir_new.php (created by Python code)
+--> 
+<b>Choose the folder you like to analysize...</b>
+<input type="radio" name="folder" value="photos">photos
+<input type="radio" name="folder" value="data">data
+<input type="radio" name="folder" value="music">music
+<input type="radio" name="folder" value="movie">movie
